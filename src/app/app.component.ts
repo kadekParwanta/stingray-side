7e324b352614a37b6381c73c39d8213583d7a0b7
Mitrais <mitrais@Mitraiss-iMac.local>
2016-12-27 15:30:41 +0800

    Add grid

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,7 +25,7 @@ export class MyApp {
     // used for an example of ngFor and navigation
     this.pages = [
       { title: 'Home', component: HomePage },
-      { title: 'Sekolah', component: SchoolsPage }
+      { title: 'Yearbook', component: SchoolsPage }
     ];
 
   }
--- src/pages/schools/schools.ts
+++ src/pages/schools/schools.ts
@@ -16,6 +16,9 @@ import { SchoolApi } from '../../app/shared/sdk/services';
 export class SchoolsPage {
 
   private schools = new Array<School>()
+  filteredSchools = new Array<School>()
+  grid: Array<Array<School>>
+  searchQuery: string = ''
 
   constructor(public navCtrl: NavController, public navParams: NavParams, private schoolApi: SchoolApi) {
     this.getSchools()
@@ -26,15 +29,15 @@ export class SchoolsPage {
   }
 
   getSchools() {
-    this.schoolApi.find({include:'photos'}).subscribe(
-      (schools:Array<School>) => {
-        
+    this.schoolApi.find({ include: ['photos','generations']}).subscribe(
+      (schools: Array<School>) => {
+
         for (var i = 0; i < schools.length; i++) {
           var schoolData = schools[i]
           var photos = schoolData.photos
           if (photos.length > 0) {
             var photo = photos[0].url
-            schoolData.photos[0].url = 'http://ec2-35-160-136-100.us-west-2.compute.amazonaws.com'+photo
+            schoolData.photos[0].url = 'http://ec2-35-160-136-100.us-west-2.compute.amazonaws.com' + photo
           } else {
             var media = new Media()
             media.url = 'http://ec2-35-160-136-100.us-west-2.compute.amazonaws.com/storages/missing/placeholder.jpg'
@@ -45,9 +48,45 @@ export class SchoolsPage {
 
           this.schools.push(schoolData)
         }
+        this.filteredSchools = this.schools
+        this.iterateSchool()
 
       }
     )
+  }
+
+  iterateSchool() {
+    let rowNum = 0; //counter to iterate over the rows in the grid
+    this.grid = Array(Math.ceil(this.filteredSchools.length / 2));
+    for (let i = 0; i < this.filteredSchools.length; i += 2) { //iterate schools
+
+      this.grid[rowNum] = Array(2); //declare two elements per row
+
+      if (this.filteredSchools[i]) { //check file URI exists
+        this.grid[rowNum][0] = this.filteredSchools[i] //insert image
+      }
+
+      if (this.filteredSchools[i + 1]) { //repeat for the second image
+        this.grid[rowNum][1] = this.filteredSchools[i + 1]
+      } else {
+        this.grid[rowNum].length = 1
+      }
+
+      rowNum++; //go on to the next row
+    }
+  }
+
+  getItems(ev: any) {
+    // set val to the value of the searchbar
+    let val = ev.target.value;
+
+    // if the value is an empty string don't filter the items
+    if (val && val.trim() != '') {
+      this.filteredSchools = this.schools.filter((item) => {
+        return (item.name.toLowerCase().indexOf(val.toLowerCase()) > -1);
+      })
+      this.iterateSchool()
+    }
   }
 
 }
