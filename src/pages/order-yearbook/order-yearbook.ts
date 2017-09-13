361aa82729409dd13c5da46fafad71305383328e
Kadek Juliana Parwanta <kadek.parwanta@gmail.com>
2017-09-13 15:25:53 +0800

    create new yearbook order

--- src/pages/order-yearbook/order-yearbook.ts
+++ src/pages/order-yearbook/order-yearbook.ts
@@ -1,7 +1,8 @@
 import { Component } from '@angular/core';
-import { NavController, NavParams } from 'ionic-angular';
-import { Generation } from '../../app/shared/sdk/models';
-import { GenerationApi } from '../../app/shared/sdk/services';
+import { NavController, NavParams, LoadingController, AlertController } from 'ionic-angular';
+import { Generation, Order, User } from '../../app/shared/sdk/models';
+import { GenerationApi, OrderApi } from '../../app/shared/sdk/services';
+import { UserData } from '../../providers/user-data';
 
 /*
   Generated class for the OrderYearbook page.
@@ -15,21 +16,55 @@ import { GenerationApi } from '../../app/shared/sdk/services';
 })
 export class OrderYearbookPage {
   generation: Generation
+  order: Order = new Order()
 
-  constructor(public navCtrl: NavController, public navParams: NavParams,  public generationApi: GenerationApi) {
-    this.generation = navParams.get('generation');
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams,  
+    public generationApi: GenerationApi,
+    public userData: UserData,
+    public orderApi: OrderApi,
+    public alertCtrl: AlertController,
+    public loadingCtrl: LoadingController) {
+      this.generation = navParams.get('generation');
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad OrderYearbookPage');
+    this.userData.getUser().then((user: User)=>{
+      this.order.type = "YEARBOOK"
+      this.order.userId = user.id
+      //TODO price should be validated or created from server
+      this.order.price = 1.1 * this.generation.price
+    })
   }
 
   submit(event) {
-    
+    let loading = this.loadingCtrl.create({
+      content: 'Please Wait...'
+    });
+
+    loading.present();
+    this.orderApi.create(this.order).subscribe(res => {
+      this.orderApi.linkYearbooks(res.id, this.generation.id).subscribe(resp => {
+        loading.dismiss()
+        this.back()
+        this.showAlert("Sukses","Pesanan anda sedang diproses.",["OK"])
+      })
+    })
   }
 
   back() {
     this.navCtrl.pop()
+  }
+
+  showAlert(title: string, subTitle: string, buttons: Array<string>) {
+    let alert = this.alertCtrl.create({
+      title: title,
+      subTitle: subTitle,
+      buttons: buttons
+    });
+    alert.present();
   }
 
 }
