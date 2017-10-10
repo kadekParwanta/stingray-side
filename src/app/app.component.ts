d7f2ce1b0346c51ac54316c3ffa497a098147d63
Kadek Juliana Parwanta <Kadek_P@mitrais.com>
2017-10-10 13:54:51 +0800

    fix bug this.me is undefined

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -46,7 +46,7 @@ export class MyApp implements OnDestroy {
     } else {
       this.chatService.leave(this.singleRoom.name)
     }
-    
+
   }
 
   @ViewChild(Nav) nav: Nav;
@@ -244,55 +244,55 @@ export class MyApp implements OnDestroy {
     let userId = this.loopbackAuth.getCurrentUserId()
 
     this.chatService.authenticate({ id: accessTookenId, userId: userId })
-    .subscribe(()=> {
-      if (isAdmin) {
-        this.roomApi.find().subscribe((rooms:[Room]) => {
-          this.rooms = rooms
-          rooms.forEach(room => {
-            this.chatService.join(room.name)
-            this.chatService.listenNewMessage(room.name)
-            this.listenToNewMessageEvents(room)
-          })
-        })
-        this.messageApi.updateAll({status: "pending", userId: {neq : userId}},{status:"delivered"}).subscribe(res => {
-          console.log('updated messages from pending to delivered: ' + res.count)
-        })
-      } else {
+      .subscribe(() => {
         this.userData.getUser().then((user: User) => {
           if (user) {
             this.me = user
-            let role = user.roleName as any
-            this.isAdmin = (role == "admin")
-            let roomName = this.me.username
-            this.chatService.join(roomName).subscribe((room: Room) => {
-              this.singleRoom = room
-              this.userData.room(room)
-              this.chatService.listenNewMessage(roomName)
-              this.listenToNewMessageEvents(room)
-              this.messageApi.updateAll({status: "pending", roomId: room.id, userId: {neq : this.me.id}},{status:"delivered"}).subscribe(res => {
-                console.log('updated messages from pending to delivered: ' + res.count + ' roomId= ' + room.id)
+            this.isAdmin = isAdmin
+            if (isAdmin) {
+              //Admins   subscribe all rooms
+              this.roomApi.find().subscribe((rooms: [Room]) => {
+                this.rooms = rooms
+                rooms.forEach(room => {
+                  this.chatService.join(room.name)
+                  this.chatService.listenNewMessage(room.name)
+                  this.listenToNewMessageEvents(room)
+                })
               })
-            })
+              this.messageApi.updateAll({ status: "pending", userId: { neq: userId } }, { status: "delivered" }).subscribe(res => {
+                console.log('updated messages from pending to delivered: ' + res.count)
+              })
+            } else {
+              //Users subscribe their own room
+              let roomName = this.me.username
+              this.chatService.join(roomName).subscribe((room: Room) => {
+                this.singleRoom = room
+                this.userData.room(room)
+                this.chatService.listenNewMessage(roomName)
+                this.listenToNewMessageEvents(room)
+                this.messageApi.updateAll({ status: "pending", roomId: room.id, userId: { neq: this.me.id } }, { status: "delivered" }).subscribe(res => {
+                  console.log('updated messages from pending to delivered: ' + res.count + ' roomId= ' + room.id)
+                })
+              })
+            }
           }
         })
-        
-      }
-    })
+      })
   }
 
   listenToNewMessageEvents(room: Room) {
-    this.events.subscribe('new-message-'+room.name,(message: Message) => {
+    this.events.subscribe('new-message-' + room.name, (message: Message) => {
       console.log("new message", message)
       if (message.userId != this.me.id) {
         this.localNotifications.schedule({
           id: 1,
           text: message.text
         });
-        this.messageApi.updateAttributes(message.id, {status: 'delivered'}).subscribe(res => {
-          console.log('update to delivered id= '+ message.id)
-        }) 
+        this.messageApi.updateAttributes(message.id, { status: 'delivered' }).subscribe(res => {
+          console.log('update to delivered id= ' + message.id)
+        })
       }
     })
-    
+
   }
 }
